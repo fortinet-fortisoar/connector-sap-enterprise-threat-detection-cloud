4dd2232492ea5d979b2ffe436f18292da6ede5fe
randomstr1ng <julian@petersohn.it>
2024-06-20 10:18:00 +0200

    update copyright

--- sap-etd-cloud/constants.py
+++ sap-etd-cloud/constants.py
@@ -1,3 +1,9 @@
+"""
+Copyright start
+MIT License
+Copyright (c) 2024 Fortinet Inc
+Copyright end
+"""
 LOGGER_NAME = 'sap-etd-cloud'
 REQUEST_TIMEOUT = 600
 AUTH_API_ENDPOINT = '/oauth/token?grant_type=client_credentials'
